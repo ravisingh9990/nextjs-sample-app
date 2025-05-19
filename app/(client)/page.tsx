b64eda4a61d994edd136ec15555f50f63db2df13
Ravi Singh <ravisingh9990@gmail.com>
2025-05-19 23:47:15 +0530

    page insigts setup

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,12 +1,15 @@
 import Container from "@/components/Container";
 import HomeBanner from "@/components/new/HomeBanner";
 import ProductGrid from "@/components/ProductGrid";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <Container className="py-10">
       <HomeBanner />
       <ProductGrid />
+      <SpeedInsights/>
     </Container>
+    
   );
 }
